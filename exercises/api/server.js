2b4c15241a0ab9d3f47f52a635f170cb1cf42c26
JustinSHong <jhong2009@gmail.com>
2019-06-30 18:16:50 -0400

    made findAsset read file asynchronously

--- exercises/api/server.js
+++ exercises/api/server.js
@@ -7,9 +7,18 @@ const path = require("path");
  * this function is blocking, fix that
  * @param {String} name full file name of asset in asset folder
  */
-const findAsset = name => {
+const findAsset = async name => {
     const assetPath = path.join(__dirname, "assets", name);
-    return fs.readFileSync(assetPath, { encoding: "utf-8" }).toString();
+    console.log("assetPath", assetPath);
+    // return fs.readFileSync(assetPath, { encoding: "utf-8" }).toString();
+    return await fs.readFile(
+        assetPath,
+        { encoding: "utf-8" },
+        (error, result) => {
+            if (error) console.error(error);
+            return result;
+        }
+    );
 };
 
 const hostname = "127.0.0.1";
@@ -19,13 +28,14 @@ const port = 3000;
 const logRequest = (method, route, status) =>
     console.log(method, route, status);
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const method = req.method;
     const route = url.parse(req.url).pathname;
+    console.log("route", route);
     // this is sloppy, espcially with more assets, create a "router"
     if (route === "/") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(findAsset("index.html"));
+        res.writeHead("200", { "Content-Type": "text/html" });
+        res.write(await findAsset("index.html"));
         logRequest(method, route, 200);
         res.end();
     } else {
